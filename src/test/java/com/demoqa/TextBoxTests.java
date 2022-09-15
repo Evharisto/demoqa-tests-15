b71f0eba74db7ae538854189b69c1a16a7bff2da
Evharisto <sv.lisitskaya@mail.ru>
2022-09-15 21:42:17 +0300

    add test1

---
+++ src/test/java/com/demoqa/TextBoxTests.java
@@ -0,0 +1,25 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void fillTormTest() {
+        open("/text-box");
+        $("[id=userName]").setValue("Egor");
+
+    }
+}
