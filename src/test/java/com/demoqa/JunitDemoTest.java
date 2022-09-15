8043c5729c986be651b24e34bf597b9bcc1fcbfd
Evharisto <sv.lisitskaya@mail.ru>
2022-09-15 20:51:12 +0300

    add test

---
+++ src/test/java/com/demoqa/JunitDemoTest.java
@@ -0,0 +1,31 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class JunitDemoTest {
+
+    @BeforeAll
+    static void setUp() {
+        System.out.println("### @BeforeAll !");
+    }
+
+    @Test
+    void simpleTest1() {
+        System.out.println("###     @Test simpleTest1() !");
+        Assertions.assertTrue(3>2);
+    }
+
+    @Test
+    void simpleTest2() {
+        System.out.println("###     @Test simpleTest2() !");
+        Assertions.assertTrue(3>1);
+    }
+
+    @AfterAll
+    static void tearDown() {
+        System.out.println("### @AfterAll !");
+    }
+}
