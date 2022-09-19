16f181c30bcedba76806ee6f6432f737c7290506
Evharisto <sv.lisitskaya@mail.ru>
2022-09-19 11:51:25 +0300

    updated and added checking

--- src/test/java/com/demoqa/TextBoxTests.java
+++ src/test/java/com/demoqa/TextBoxTests.java
@@ -1,5 +1,6 @@
 package com.demoqa;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -32,7 +33,25 @@ public class TextBoxTests {
         $(".react-datepicker__day--019").click();
         $("#subjectsInput").setValue("Computer Science").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
-      //  $("#submit").click();
+        $("#uploadPicture").uploadFromClasspath("image.jpg");
+        $("#currentAddress").setValue("Calgary");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $$(".table-responsive").findBy(text("Student Name")).shouldHave(text("Ivan Ivanov"));
+        $$(".table-responsive").findBy(text("Student Email")).shouldHave(text("ivanivanov@gmail.com"));
+        $$(".table-responsive").findBy(text("Gender")).shouldHave(text("Male"));
+        $$(".table-responsive").findBy(text("Mobile")).shouldHave(text("8800555353"));
+        $$(".table-responsive").findBy(text("Date of Birth")).shouldHave(text("19 February,1971"));
+        $$(".table-responsive").findBy(text("Subjects")).shouldHave(text("Computer Science"));
+        $$(".table-responsive").findBy(text("Hobbies")).shouldHave(text("Sports"));
+        $$(".table-responsive").findBy(text("Picture")).shouldHave(text("image.jpg"));
+        $$(".table-responsive").findBy(text("Address")).shouldHave(text("Calgary"));
+        $$(".table-responsive").findBy(text("State and City")).shouldHave(text("NCR Delhi"));
 
 
     }
